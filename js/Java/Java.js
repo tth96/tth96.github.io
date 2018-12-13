9a22a694d1b3abc3e9506c6eae61a1fcbd6e43be
kalshyn1410 <kalshyn1410@gmail.com>
2018-12-13 12:13:46 +0700

    sua loi jquery backtotop

--- js/Java/Java.js
+++ js/Java/Java.js
@@ -14,11 +14,21 @@ jQuery(function($){
 		});
 	}
 	InoxAnhTrang.BackToTop = function(){
+    	if($("#backtotop").length > 0){
+		$(window).scroll(function () {
+			var e = $(window).scrollTop();
+			if (e > 400) {
+				$("#backtotop").show()
+			} else {
+				$("#backtotop").hide()
+			}
+		});
 		$("#backtotop").click(function () {
 			$('body,html').animate({
 				scrollTop: 0
 			})
-		})		
+		})
+	}		
 	}
 	$(document).ready(function(){
 		InoxAnhTrang.MainSilder();
