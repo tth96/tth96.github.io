1705c34a83f3bbc70bd567a14d412fd1af2f2532
kalshyn1410 <kalshyn1410@gmail.com>
2018-12-13 11:50:31 +0700

    loi backtop

--- js/Java/Java.js
+++ js/Java/Java.js
@@ -1,20 +1,28 @@
 jQuery(function($){
 	"use strict";
-	var InoxAnhTrang = window.InoxAnhTrang || {};
+	var InoxAnhTrang = {};
 
 	InoxAnhTrang.MainSilder = function(){
 		$('.main-slider').slick({
 			slidesToShow: 1,
-			// autoplay: true,
- 			autoplaySpeed: 5000,
+			autoplay: true,
+ 			autoplaySpeed:4000,
  			dots: true,
  			arrows: false,
- 			
+
 
 		});
 	}
+	InoxAnhTrang.BackToTop = function(){
+		$("#backtotop").click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			})
+		})		
+	}
 	$(document).ready(function(){
 		InoxAnhTrang.MainSilder();
+		InoxAnhTrang.BackToTop();
 
 	});
 	$(window).on('load', function(){
