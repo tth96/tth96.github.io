b52c898294efdf72a6f8bf569b7903c3e0d8b4ab
kalshyn1410 <kalshyn1410@gmail.com>
2019-01-02 15:53:33 +0700

    add BT slide

--- Game01/PlayingGame.js
+++ Game01/PlayingGame.js
@@ -1,6 +1,6 @@
 // var diem , diemkhichoi , playerdangchoi , gameplaying;
 // document.querySelector(".btn-roll").addEventListener('click', function(){
-	
+
 // 		var dice01 = Math.floor(Math.random()*6) + 1;
 // 		var dice02 = Math.floor(Math.random()*6) + 1;
 // 		document.getElementById('dice-1').style.display = "block";
@@ -9,63 +9,74 @@
 // 		document.getElementById('dice-2').src = 'dice-' + dice02 + '.png';
 // 		diemkhichoi = dice01 + dice02;
 // 		document.querySelector('#current-0').textContent = diemkhichoi; 
-		
-	
+
+
 // })
-jQuery(function($){
+jQuery(function ($) {
 	"use strict";
 	var GameHTML = {};
-	var roundscore,score;
+	var roundscore, score;
 	var winner;
 	var checkplayer;
-	GameHTML.NewGame = function(){
-		$(".btn-new").click(function(){
-			$("#dice-1").css("display","none");
-			$("#dice-2").css("display","none");
-			$(".player-current-score").text(0);
-			$(".player-score").text(0);
-			$(".player-0-panel").addClass("active");
-			$(".player-1-panel").removeClass("active");
-			$(".player-"+ checkplayer +"-panel").removeClass("winner");
-			$("#name-0").text("PLAYER 1");
-			$("#name-1").text("PLAYER 2");
-			// $("#name-"+ checkplayer).css("padding-bottom","0px");
-			$(".player-score").css("display","block");
-			GameHTML.RollDice();
-			GameHTML.Hold();
-
+	swal("Luật chơi!", "Xoay xuc sac. Nguoi choi mac dinh la nguoi choi dau tien", "info");
+	GameHTML.NewGame = function () {
+		$(".btn-new").click(function () {
+			//debugger;
+			GameHTML.Default();
+			$(".player-score").css("display", "block");
 		});
 	}
-	GameHTML.RollDice = function(){
-		$(".btn-roll").click(function(){
-			var dice01 = Math.floor(Math.random()*6) + 1;
-			var dice02 = Math.floor(Math.random()*6) + 1;
-			$("#dice-1").css("display","block");
-			$("#dice-2").css("display","block");
+	GameHTML.Default = function () {
+		//debugger;
+		$("#dice-1").css("display", "none");
+		$("#dice-2").css("display", "none");
+		$(".player-score").text('0');
+		$(".player-current-score").text('0');
+		$(".player-0-panel").addClass("active");
+		$(".player-1-panel").removeClass("active");
+		$(".player-0-panel").removeClass("winner");
+		$(".player-1-panel").removeClass("winner");
+		$("#name-0").text("PLAYER 1");
+		$("#name-1").text("PLAYER 2");
+		winner = null;
+		checkplayer = 0;
+		roundscore = 0;
+		score = [0, 0]
+	}
+	GameHTML.RollDice = function () {
+		$(".btn-roll").click(function () {
+			//debugger;
+			if(winner !== null) return;
+			//if(winner) return;
+			var dice01 = Math.floor(Math.random() * 6) + 1;
+			var dice02 = Math.floor(Math.random() * 6) + 1;
+			$("#dice-1").css("display", "block");
+			$("#dice-2").css("display", "block");
 			$('#dice-1').attr('src', 'dice-' + dice01 + '.png');
 			$('#dice-2').attr('src', 'dice-' + dice02 + '.png');
-				roundscore = roundscore +(dice01 + dice02);
-			if(dice01 !== 1 && dice02 !== 1){
+			roundscore = roundscore + (dice01 + dice02);
+			if (dice01 !== 1 && dice02 !== 1) {
 				$("#current-" + checkplayer).text(roundscore);
 			} else {
+				swal("Tieu de!", `Nguoi choi so ${checkplayer + 1} xoay phai xuc sac so 1. Doi luot choi`, "info");
 				GameHTML.nguoichoiketiep();
 			}
 		})
 	}
-	GameHTML.nguoichoiketiep = function(){
-			if (checkplayer == 0){
-				checkplayer = 1;
-			} else{
-				checkplayer = 0;
-			}
-			roundscore = 0;
-			$("#current-0").text(roundscore);
-			$("#current-1").text(roundscore);
-			$(".player-0-panel").toggleClass("active");
-			$(".player-1-panel").toggleClass("active");
-
-
-		}
+	GameHTML.nguoichoiketiep = function () {
+		//debugger;
+		// if (checkplayer == 0) {
+		// 	checkplayer = 1;
+		// } else {
+		// 	checkplayer = 0;
+		// }
+		checkplayer === 1 ? checkplayer = 0 : checkplayer = 1;
+		roundscore = 0;
+		$("#current-0").text(0);
+		$("#current-1").text(0);
+		$(".player-0-panel").toggleClass("active");
+		$(".player-1-panel").toggleClass("active");
+	}
 	// GameHTML.nguoichoiketiep = function(){
 	// 	$(".player-0-panel").toggleClass("active");
 	// 	$(".player-1-panel").toggleClass("active");
@@ -73,57 +84,44 @@ jQuery(function($){
 	// 	if(so0orso1 == 0){
 	// 		so0orso1 = 1;
 	// 	} else {
-		// so0orso1 = 0;
+	// so0orso1 = 0;
 	// }
 	// }
 
-	GameHTML.Hold = function(){
-		$(".btn-hold").click(function(){
-			if(checkplayer == 0){
-				score[0] = score[0] + roundscore;
-				$("#score-" + checkplayer).text(score[0]);
-			} else {
-				score[1] = score[1] + roundscore;
-				$("#score-" + checkplayer).text(score[1]);
-			}
+	GameHTML.Hold = function () {
+		$(".btn-hold").click(function () {
+			//debugger;
+			if(winner !== null) return;
+
+			score[checkplayer] = score[checkplayer] + roundscore;
+			$("#score-" + checkplayer).text(score[checkplayer]);
+
 			// score[$("#score-" + checkplayer).val()] = score[$("#score-" + checkplayer).val()] + roundscore;
 			// score['checkplayer'] = score['checkplayer'] + roundscore;
 			// score[$("#score-" + checkplayer)] = roundscore + score[$("#score-" + checkplayer)];
 			var giatrinhanvao = $("input").val();
-			if(giatrinhanvao){
-				winner = giatrinhanvao;
-			} else{
-			winner = 100;
-		}
-		if(score >= winner){
-			$(".player-"+ checkplayer +"-panel").addClass("winner");
-			$(".player-"+ checkplayer +"-panel").removeClass("active");
-			$("#dice-1").css("display","none");
-			$("#dice-2").css("display","none");
-			// $(".player-score").css("display","none");
-			$("#name-"+ checkplayer).text("WINNER!");
-			// $("#name-"+ checkplayer).css("padding-bottom","50px");
-			$(".btn-roll").unbind('click');
-			$(".btn-hold").unbind('click');
-			
+			// if (giatrinhanvao) {
+			// 	giatrinhanvao = parseInt(giatrinhanvao);
+			// } else {
+			// 	giatrinhanvao = 100;
+			// }
+			giatrinhanvao =  giatrinhanvao ? parseInt(giatrinhanvao) : 100;
+
+			if (score[checkplayer] >= giatrinhanvao) {
+				winner = checkplayer;
+				$(".player-" + checkplayer + "-panel").addClass("winner");
+				$(".player-" + checkplayer + "-panel").removeClass("active");
+				$("[id='dice-']").css("display", "none");
+				// $(".player-score").css("display","none");
+				$("#name-" + checkplayer).text("WINNER!");
+
+				// $("#name-"+ checkplayer).css("padding-bottom","50px");
 			} else {
-			GameHTML.nguoichoiketiep();
-		}
+				GameHTML.nguoichoiketiep();
+			}
 		});
 	}
-	GameHTML.Default = function(){
-			$("#dice-1").css("display","none");
-			$("#dice-2").css("display","none");
-			$(".player-score").text('0');
-			$(".player-current-score").text('0');
-			$(".player-0-panel").addClass("active");
-			$(".player-1-panel").removeClass("active");
-			$(".player-0-panel").removeClass("winner");
-			$(".player-1-panel").removeClass("winner");
-			roundscore = 0;
-			score = {0:0, 1:0};
-			console.log(score);
-	}
+	
 	// GameHTML.AfterWinner = function(){
 	// 	if($(".player-"+ checkplayer +"-panel").addClass("winner")){
 	// 		return GameHTML.RollDice() = false;
@@ -135,17 +133,17 @@ jQuery(function($){
 	// 	GameHTML.RollDice() = false;
 	// 	}
 	// }
-	$(document).ready(function(){
+	$(document).ready(function () {
 		GameHTML.RollDice();
 		GameHTML.Hold();
-		GameHTML.nguoichoiketiep();
+		//GameHTML.nguoichoiketiep();
 		// GameHTML.AfterWinner();
 		GameHTML.NewGame();
 		GameHTML.Default();
 		// GameHTML.Xulyclickwinner();
 
-	
+
 	});
-	$(window).on('load',function(){
+	$(window).on('load', function () {
 	});
 });
